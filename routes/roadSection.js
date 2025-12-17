9437139c2edd7c3e3460959825d29cde5940e09e
MANDALORE\nilsg <test@gmail.com>
2025-12-17 17:36:46 +0100

    feat: add GET /{db}/road-section/:id endpoint to fetch a single road section by id

--- routes/roadSection.js
+++ routes/roadSection.js
@@ -5,4 +5,7 @@ const roadSectionController = require('../controllers/roadSection');
 // Route: /:db/road-section
 router.get('/:db/road-section', roadSectionController.getRoadSections);
 
+// Route: /:db/road-section/:id
+router.get('/:db/road-section/:id', roadSectionController.getRoadSectionById);
+
 module.exports = router;
