7586ab3cd0007ad7607d8d81cc5a0e98ef183234
MANDALORE\nilsg <test@gmail.com>
2025-12-17 18:31:13 +0100

    refactor: clean up and improve road section controller logic

--- routes/roadSection.js
+++ routes/roadSection.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const roadSectionController = require('../controllers/roadSection');
+const { getRoadSections, getRoadSectionById, getControlsByRoadSectionId, getDeparturesByRoadSectionId } = require('../controllers/roadSection');
 
 // Route: /:db/road-section
-router.get('/:db/road-section', roadSectionController.getRoadSections);
+router.get('/:db/road-section', getRoadSections);
 
 // Route: /:db/road-section/:id
-router.get('/:db/road-section/:id', roadSectionController.getRoadSectionById);
+router.get('/:db/road-section/:id', getRoadSectionById);
 
 // Route: /:db/road-section/:id/control
-router.get('/:db/road-section/:id/control', roadSectionController.getControlsByRoadSectionId);
+router.get('/:db/road-section/:id/control', getControlsByRoadSectionId);
 
 // Route: /:db/road-section/:id/departure
-router.get('/:db/road-section/:id/departure', roadSectionController.getDeparturesByRoadSectionId);
+router.get('/:db/road-section/:id/departure', getDeparturesByRoadSectionId);
 
 module.exports = router;
