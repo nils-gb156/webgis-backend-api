eb83031d50b69aa253b13b64044fde11f7c7e647
MANDALORE\nilsg <test@gmail.com>
2025-12-18 13:10:39 +0100

    eat: add GET /{db}/tree/:id endpoint to fetch a single tree by id

--- routes/tree.js
+++ routes/tree.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getTrees } = require('../controllers/tree');
+const { getTrees, getTreeById } = require('../controllers/tree');
 
 // Route: /:db/tree
 router.get('/:db/tree', getTrees);
+
+// Route: /:db/tree/:id
+router.get('/:db/tree/:id', getTreeById);
 
 module.exports = router;
