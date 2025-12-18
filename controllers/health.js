f97eda5a4522aed7bcef6ca55d823247293d8473
Nils GB <nilsgrossebeikel@gmail.com>
2025-12-18 13:34:48 +0100

    bugfix: health check uses dynamic db config and checks all pools

--- controllers/health.js
+++ controllers/health.js
@@ -1,20 +1,17 @@
-const { lohmar_pool, roetgen_pool } = require('../db');
+const dbPools = require('../db');
 
 const checkDbHealth = async (req, res) => {
-	const results = {};
-	try {
-		await lohmar_pool.query('SELECT 1');
-		results.lohmar_db = { status: 'ok', db: process.env.DB1_NAME };
-	} catch (err) {
-		results.lohmar_db = { status: 'error', error: err.message };
-	}
-	try {
-		await roetgen_pool.query('SELECT 1');
-		results.roetgen_db = { status: 'ok', db: process.env.DB2_NAME };
-	} catch (err) {
-		results.roetgen_db = { status: 'error', error: err.message };
-	}
-	res.json(results);
+  const results = {};
+  const poolNames = Object.keys(dbPools);
+  for (const name of poolNames) {
+    try {
+      await dbPools[name].query('SELECT 1');
+      results[name] = { status: 'ok', db: name };
+    } catch (err) {
+      results[name] = { status: 'error', error: err.message };
+    }
+  }
+  res.json(results);
 };
 
 module.exports = { checkDbHealth };
